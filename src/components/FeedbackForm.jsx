6e17416b085d6541bf2e05b4a562dd150fd370eb
yudhaph0300 <yudhapamungkas0300@gmail.com>
2023-08-09 09:43:28 +0700

    React Video - Real-Time Validation

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -1,10 +1,25 @@
 import { useState } from "react"
 import Card from "./shared/Card"
+import Button from "./shared/Button"
 
 function FeedbackForm() {
 
    const [text, setText] = useState('');
+   const [btnDisabled, setBtnDisabled] = useState(true)
+   const [message, setMessage] = useState('')
+
    const handleTextChange = (e) => {
+      if (text === '') {
+         setBtnDisabled(true)
+         setMessage(null)
+      } else if (text !== '' && text.trim().length <= 10) {
+         setMessage('Text must be at least 10 characters')
+         setBtnDisabled(true)
+      } else {
+         setMessage(null)
+         setBtnDisabled(false)
+      }
+
       setText(e.target.value)
    }
 
@@ -22,8 +37,10 @@ function FeedbackForm() {
                placeholder="Write a review"
                value={text}
             />
-            <button type="submit">Send</button>
+            <Button type="submit" isDisabled={btnDisabled}>Send</Button>
          </div>
+         
+         {message && <div className="message">{message}</div>}
       </form>
     </Card>
   )
