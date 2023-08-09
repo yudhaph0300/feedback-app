e3d3cd284cdc0b0f5f3c5d252a733f4458c28e69
yudhaph0300 <yudhapamungkas0300@gmail.com>
2023-08-09 09:13:16 +0700

    React Video - Form Input & State

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import FeedbackList from "./components/FeedbackList";
 import FeedbackStats from "./components/FeedbackStats";
+import FeedbackForm from "./components/FeedbackForm";
 import Header from "./components/Header";
 import FeedbackData from "./data/FeedbackData";
 
@@ -16,6 +17,7 @@ function App() {
     <>
       <Header />
       <div className="container">
+        <FeedbackForm />
         <FeedbackStats feedback={feedback} />
         <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
       </div>
