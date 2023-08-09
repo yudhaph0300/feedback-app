e34c92b3b1078df49a150f494265c33cd026aea5
yudhaph0300 <yudhapamungkas0300@gmail.com>
2023-08-09 10:09:58 +0700

    React Video - Rating Select Component

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -1,10 +1,12 @@
 import { useState } from "react"
 import Card from "./shared/Card"
 import Button from "./shared/Button"
+import RatingSelect from "./RatingSelect";
 
 function FeedbackForm() {
 
    const [text, setText] = useState('');
+   const [rating, setRating] = useState(10);
    const [btnDisabled, setBtnDisabled] = useState(true)
    const [message, setMessage] = useState('')
 
@@ -29,7 +31,7 @@ function FeedbackForm() {
     <Card>
       <form action="">
          <h2>How would you rate your service with us?</h2>
-         {/* @todo - rating select component */}
+         <RatingSelect select={(rating) => setRating(rating)}/>
          <div className="input-group">
             <input
                onChange={handleTextChange}
